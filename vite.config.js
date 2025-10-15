1f36db73d4d46abc611036f864d825d46f87f601
Euphoria <1534554535@qq.com>
2025-10-15 09:57:27 +0800

    简单修改

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig(({ mode, command }) => {
     },
     // vite 相关配置
     server: {
-      port: 8001,
+      port: 80,
       host: true,
       open: true,
       proxy: {
